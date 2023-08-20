6cfd1c1d0b723a9fa9d22625b6f9140502693568
Charles Chavis <chavisca@gmail.com>
2023-08-20 05:20:27 -0400

    fixed clog pathing again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('/helpers/clog');
+const { clog } = require('./helpers/clog');
 const api = require('/routes/index.js');
 
 const PORT = process.env.PORT || 3001;
