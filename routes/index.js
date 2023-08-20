f953bdfe4b1198b9bbcb93a3af13abcb96e32837
Charles Chavis <chavisca@gmail.com>
2023-08-20 05:29:12 -0400

    reattempt at correcting syntax for pathing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 // Import our modular routers for /tips and /feedback
-const notesRouter = require('/public/pages/notes');
-const indexRouter = require('/public/pages/index');
+const notesRouter = require('./notes');
+const indexRouter = require('./index');
 
 const app = express();
 
