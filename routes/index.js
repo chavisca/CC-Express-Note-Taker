fb3de14b67979c69a7d2eb7c578972c5d9db3f84
Charles Chavis <chavisca@gmail.com>
2023-08-22 03:13:28 -0400

    fixed modular routes to pull db JSON data at api/notes route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
+const { readFromFile } = require('../helpers/fsUtils');
+const router = express.Router();
 // Import our modular routers for /tips and /feedback
 const notesRouter = require('./notes');
 
 const app = express();
 
+router.get('/notes', (req, res) => {
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)))
+    .catch((error) => res.status(500).json({ error: 'Internal server error'}));
+});
+
 app.use('/notes', notesRouter);
 
 module.exports = app;
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const { clog } = require('./helpers/clog.js');
 const api = require('./routes/index.js');
-// const notesRouter = require('./routes/notes.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,7 +12,6 @@ app.use(clog);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// app.use('/notes', notesRouter);
 app.use('/api', api);
 
 // GET route for the index page
