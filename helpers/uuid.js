e133a9ea4b652d4d908586eaa3e5a3928c8303a2
Charles Chavis <chavisca@gmail.com>
2023-08-23 01:18:18 -0400

    trying to globally use uuid

--- helpers/uuid.js
+++ helpers/uuid.js
@@ -1,5 +1,6 @@
 // Immediately export a function that generates a string of random numbers and letters
-module.exports = () =>
-  Math.floor((1 + Math.random()) * 0x10000)
+function uuid() {
+  return Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
+};
