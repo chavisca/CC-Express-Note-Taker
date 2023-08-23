46c452023bab01ed0d537f89be13ac25b0dd9f39
Charles Chavis <chavisca@gmail.com>
2023-08-23 01:54:21 -0400

    fixing notes db routing

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const { readFromFile } = require('../helpers/fsUtils');
-const router = express.Router();
 // Import our modular routers for /tips and /feedback
 const notesRouter = require('./notes');
 
 const app = express();
 
-router.get('/notes', (req, res) => {
+app.get('/notes', (req, res) => {
     readFromFile('./db/db.json')
     .then((data) => res.json(JSON.parse(data)))
     .catch((error) => res.status(500).json({ error: 'Internal server error'}));
 });
 
-router.use('/notes', notesRouter);
+app.use('/notes', notesRouter);
 
-module.exports = router;
+module.exports = app;
